aeac09247647f8cdc80154150ea1de068e60396e
NhatNK-43 <khanh.nhat.365@gmail.com>
2023-12-10 09:21:05 +0700

    ok

---
+++ fashion_shop_management/src/components/payment/Payment.js
@@ -0,0 +1,8 @@
+export function Payment() {
+    return (
+        <>
+            <h1 className="text-danger">Đây là trang thanh toán</h1>
+            <i className="bi bi-alarm"></i>
+        </>
+    )
+}
