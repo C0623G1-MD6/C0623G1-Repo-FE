bcc353bb5dbe233dfcf6120638a8a24164e9f4d3
LyDoan0302 <lydoan.kt@gmail.com>
2023-12-14 17:34:40 +0700

    [LyDTH_up_home_component]

--- fashion_shop_management/src/App.js
+++ fashion_shop_management/src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import {Route, Routes} from "react-router-dom";
-import {NotFound} from "./components/NotFound";
-import {ToastContainer} from "react-toastify";
-import {Payment} from "./components/payment/Payment";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { NotFound } from "./components/NotFound";
+import { ToastContainer } from "react-toastify";
+import { Payment } from "./components/payment/Payment";
+import Home from "./components/home/Home";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/sale-staff/payment" element={<Payment/>}></Route>
-                <Route path="*" element={<NotFound/>}></Route>
-            </Routes>
-            <ToastContainer></ToastContainer>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/sale-staff/payment" element={<Payment />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+      <ToastContainer></ToastContainer>
+    </>
+  );
 }
 
 export default App;
