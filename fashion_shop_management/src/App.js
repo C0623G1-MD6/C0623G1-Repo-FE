93e649313fbbfd30c00fb152c5db54f492e81086
LyDoan0302 <lydoan.kt@gmail.com>
2023-12-15 16:40:27 +0700

    [LyDTH_up_home_package]

--- fashion_shop_management/src/App.js
+++ fashion_shop_management/src/App.js
@@ -3,13 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import { NotFound } from "./components/NotFound";
 import { ToastContainer } from "react-toastify";
 import { Payment } from "./components/payment/Payment";
-import Home from "./components/home/Home";
+import HomePage from "./components/home/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/home" element={<Home />}></Route>
+        <Route path="/home" element={<HomePage />}></Route>
         <Route path="/sale-staff/payment" element={<Payment />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
