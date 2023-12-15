be4d4043a1b2d6a344f4f947de9ee4f0f7c46905
NhatNK-43 <khanh.nhat.365@gmail.com>
2023-12-15 16:46:47 +0700

    commit to merge

--- fashion_shop_management/src/App.js
+++ fashion_shop_management/src/App.js
@@ -3,12 +3,14 @@ import {Route, Routes} from "react-router-dom";
 import {NotFound} from "./components/NotFound";
 import {ToastContainer} from "react-toastify";
 import {Payment} from "./components/payment/Payment";
+import {LookUpCustomer} from "./components/payment/LookUpCustomer";
 
 function App() {
     return (
         <>
             <Routes>
                 <Route path="/sale-staff/payment" element={<Payment/>}></Route>
+                <Route path="/sale-staff/look-up-customer" element={<LookUpCustomer/>}></Route>
                 <Route path="*" element={<NotFound/>}></Route>
             </Routes>
             <ToastContainer></ToastContainer>
