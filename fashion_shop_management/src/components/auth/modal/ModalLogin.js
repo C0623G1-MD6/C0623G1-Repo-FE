5167816d0ae56320c0303376d41f035cebb785d2
phamthanhx18 <138504962+phamthanhx18@users.noreply.github.com>
2023-12-17 11:34:46 +0700

    thanh merge fe

--- fashion_shop_management/src/components/auth/modal/ModalLogin.js
+++ fashion_shop_management/src/components/auth/modal/ModalLogin.js
@@ -126,12 +126,12 @@ function ModalLogin() {
         }
     }
     if (user !== null) {
-        return <><Link to="/dashboard" className="btn btn-primary">Dashboard</Link> </>
+        return <><Link to="/dashboard" className="btn-dashboard btn-primary">{user.username}</Link> </>
     }
     return (
         <>
 
-            <Button onClick={handleShow}>
+            <Button className="btn-login" onClick={handleShow}>
                 <svg xmlns="http://www.w3.org/2000/svg" width={35} height={35} fill="currentColor"
                      className="bi bi-person-circle" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
