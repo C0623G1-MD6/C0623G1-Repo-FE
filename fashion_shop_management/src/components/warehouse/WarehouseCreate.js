289b410c6ff2f298b927781195b5dbf5ee0e4e23
TUẤN <tranvanlamqbdl@gmail.com>
2023-12-18 12:23:07 +0700

    lam up

--- fashion_shop_management/src/components/warehouse/WarehouseCreate.js
+++ fashion_shop_management/src/components/warehouse/WarehouseCreate.js
@@ -1,137 +1,218 @@
-export function WarehouseCreate(){
-    return(
-        <>
-    <div class="container  mt-3 p-5 form-control">
-      <div class="d-flex justify-content-center">
-        <div class="input-card form-control shadow-lg ">
-          <h2 class="input-title">Nhập Kho</h2>
-          <form class="input-form">
-            <div class="input-group">
-              <input
-                placeholder=""
-                type="text"
-                name="ngaydang"
-                id="validationDefault01"
-                disabled
-              />
-              <label for="validationDefault01">Mã phiếu</label>
-            </div>
-            <div class="input-group">
-              <input
-                placeholder=""
-                type="text"
-                name="ngaydang"
-                id="validationDefault01"
-                disabled
-              />
-              <label for="validationDefault01">Người nhập</label>
-            </div>
-            <div class="input-group">
-              <input
-                placeholder=""
-                type="text"
-                name="ngaydang"
-                id="validationDefault01"
-                disabled
-              />
-              <label for="validationDefault01">Ngày nhập kho</label>
-            </div>
-            <div class="input-group">
-              
-              <input
-                
-                list="datalistOptions"
-                id="exampleDataList"
-                placeholder=""
-              />
-              <label for="exampleDataList" 
-                >Tên sản phẩm</label
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./lam.css";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as WarehouseReceiptService from "../../services/WarehouseReceiptService";
+
+export function WarehouseCreate() {
+  const [inputValues, setInputValues] = useState({});
+  const [items, setItems] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [code, setCode] = useState("");
+
+  const getAllProducts = async () => {
+    const products = await WarehouseReceiptService.getAllProducts();
+    let res = await WarehouseReceiptService.getCode();
+    setCode(res.data);
+    setProducts(products);
+    console.log(res.data);
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
+  const initValue = {
+    sizeDetailId: 0,
+    inputQuantity: 0,
+    inputPrice: 0,
+    warehouseId: 0,
+    productId: 0,
+    sizeId: 0,
+    receiptCode: code,
+    receiptDate: new Date().toLocaleDateString(),
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setItems((prevItems) => [...prevItems, inputValues]);
+    setInputValues({});
+  };
+
+  if (!products) return null;
+  if (!code) return null;
+
+  return (
+      <>
+        <div class="container  mt-3 p-5 form-control">
+          <div class="d-flex justify-content-center">
+            <div class="input-card form-control shadow-lg ">
+              <h2 class="input-title">Nhập Kho</h2>
+              <Formik
+                  initialValues={initValue}
+                  onSubmit={() => {
+                    handleSubmit();
+                  }}
+                  validationSchema={Yup.object({
+                    inputQuantity: Yup.number()
+                        .typeError('Vui lòng chỉ nhập số')
+                        .integer("Vui lòng nhập số nguyên dương")
+                        .required("Vui lòng không để trống trường này")
+                        .min(1, "Vui lòng nhập số lớn hơn 0")
+                        .max(2000, "Vui lòng không nhập số lớn hơn 2000"),
+                    inputPrice: Yup.number()
+                        .required("Vui lòng không để trống trường này")
+                        .min(1, "Vui lòng nhập số lớn hơn 0")
+                        .max(1000000000, "Vui lòng không nhập số lớn hơn 10000000"),
+                  })}
               >
-              <datalist id="datalistOptions">
-                <option value=""></option>
-                <option value="San Francisco"></option>
-                <option value="New York"></option>
-                <option value="Seattle"></option>
-                <option value="Los Angeles"></option>
-                <option value="Chicago"></option>
-              </datalist>
+                <Form class="input-form">
+                  <div class="input-group">
+                    <Field
+                        placeholder=""
+                        type="text"
+                        name="receiptCode"
+                        id="validationDefault01"
+                        disabled
+                        onChange={handleChange}
+                    />
+                    <label for="validationDefault01">Mã phiếu</label>
+                  </div>
+                  <div class="input-group">
+                    <Field
+                        placeholder=""
+                        type="text"
+                        name="receiptDate"
+                        id="validationDefault01"
+                        onChange={handleChange}
+                        disabled
+                    />
+                    <label for="validationDefault01">Ngày nhập kho</label>
+                  </div>
+                  <div class="input-group">
+                    <Field
+                        name="productId"
+                        list="datalistOptions"
+                        id="exampleDataList"
+                        placeholder=""
+                        value={inputValues.productId}
+                        onChange={handleChange}
+                    />
+                    <label for="exampleDataList">Tên sản phẩm</label>
+                    <datalist id="datalistOptions">
+                      {products.map((product) => (
+                          <option key={product.id} value={product.productName}>
+
+                          </option>
+                      ))}
+                    </datalist>
+                    <ErrorMessage
+                        name="productId"
+                        component="span"
+                        className="err-name"
+                    ></ErrorMessage>
+                  </div>
+                  <div class="input-group">
+                    <Field
+                        placeholder=""
+                        type="text"
+                        name="sizeId"
+                        id="validationDefault01"
+                    />
+                    <label for="validationDefault01">Size</label>
+                  </div>
+                  <div class="input-group">
+                    <Field
+                        placeholder=""
+                        type="number"
+                        name="inputQuantity"
+                        id="validationDefault01"
+                        value={inputValues.inputQuantity}
+                        onChange={handleChange}
+                    />
+                    <label for="validationDefault01">Số lượng</label>
+                    <ErrorMessage
+                        name="inputQuantity"
+                        component="span"
+                        className="err-name"
+                    ></ErrorMessage>
+                  </div>
+                  <div class="input-group">
+                    <Field
+                        placeholder=""
+                        type="number"
+                        name="inputPrice"
+                        id="validationDefault01"
+                        value={inputValues.inputPrice}
+                        onChange={handleChange}
+                    />
+                    <label for="validationDefault01">Đơn giá</label>
+                    <ErrorMessage
+                        name="inputPrice"
+                        component="span"
+                        className="err-name"
+                    ></ErrorMessage>
+                  </div>
+
+                  <div class="mt-5 confirm d-flex justify-content-md-center">
+                    <div>
+                      <button
+                          type="button"
+                          class="btn btn-outline-primary rounded-0"
+                      >
+                        Thêm vào bảng
+                      </button>
+                    </div>
+                  </div>
+                </Form>
+              </Formik>
+
+              <div class="mt-5 d-flex justify-content-evenly ">
+                <table class="table">
+                  <thead class="table-secondary">
+                  <tr>
+                    <th>STT</th>
+                    <th>Mã Hàng</th>
+                    <th>Tên</th>
+                    <th>Số Lượng</th>
+                    <th>Size</th>
+                    <th>Đơn Giá</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {items.map((item, index) => (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{item.productId}</td>
+                        <td>{item.productId}</td>
+                        <td>{item.inputQuantity}</td>
+                        <td>{item.inputQuantity}</td>
+                        <td>{item.inputPrice}</td>
+                      </tr>
+                  ))}
+                  </tbody>
+                </table>
+              </div>
+              <div class="mt-5 confirm d-flex justify-content-md-center">
+                <div>
+                  <a class="btn btn-outline-secondary me-3 rounded-0">Hủy</a>
+                </div>
+                <div>
+                  <a class="btn btn-outline-primary rounded-0">Xác Nhận</a>
+                </div>
+              </div>
             </div>
-            <div class="input-group">
-              <input
-                placeholder=""
-                type="text"
-                name="ngaydang"
-                id="validationDefault01"
-              />
-              <label for="validationDefault01">Số lượng</label>
-            </div>
-            <div class="input-group">
-              <input
-                placeholder=""
-                type="text"
-                name="ngaydang"
-                id="validationDefault01"
-              />
-              <label for="validationDefault01">Size</label>
-            </div>
-          </form>
+          </div>
         </div>
-      </div>
-      
-    <div class="mt-5 confirm d-flex justify-content-md-center">
-      <div>
-        <a class="btn btn-outline-primary rounded-0">Thêm vào bảng</a>
-      </div>
-    </div>
-    <div class="mt-5 d-flex justify-content-evenly ">
-      <table class="table">
-        <thead class="table-secondary">
-          <tr>
-            <th>STT</th>
-            <th>Mã Hàng</th>
-            <th>Tên</th>
-            <th>Số Lượng</th>
-            <th>Size</th>
-            <th>Đơn Giá</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>MH-001</td>
-            <td>Quần jean</td>
-            <td>50</td>
-            <td>M,L</td>
-            <td>1.000.000</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>MH-002</td>
-            <td>Quần Kaki</td>
-            <td>50</td>
-            <td>M,L</td>
-            <td>1.000.000</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>MH-03</td>
-            <td>Quần Tây</td>
-            <td>50</td>
-            <td>M,L</td>
-            <td>1.000.000</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="mt-5 confirm d-flex justify-content-md-center">
-      <div>
-        <a class="btn btn-outline-secondary me-3 rounded-0">Hủy</a>
-      </div>
-      <div>
-        <a class="btn btn-outline-primary rounded-0">Xác Nhận</a>
-      </div>
-    </div>
-  </div>
-        </>
-    )
+      </>
+  );
 }
